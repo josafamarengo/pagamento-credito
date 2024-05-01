cb51b7e50452da1b5b22b9fbe967a235f7965c0e
Josafá Marengo <josafabmarengo@gmail.com>
2024-05-01 18:31:39 -0300

    feat: add Repository and Service classes

--- src/main/java/org/acme/repositories/PaymentRepository.java
+++ src/main/java/org/acme/repositories/PaymentRepository.java
@@ -1,2 +1,43 @@
-package org.acme.repositories;public class PaymentRepository {
+package org.acme.repositories;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.acme.models.Payment;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@ApplicationScoped
+public class PaymentRepository {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Transactional
+    public Payment create(Payment payment) {
+        entityManager.merge(payment);
+        return payment;
+    }
+
+    public List<Payment> read() {
+        return entityManager.createQuery("SELECT p FROM Payment p", Payment.class)
+                .getResultList();
+    }
+
+    public Optional<Payment> findById(UUID paymentId) {
+        return Optional.ofNullable(entityManager.find(Payment.class, paymentId));
+    }
+
+    @Transactional
+    public void delete(UUID paymentId) {
+        Payment payment = entityManager.find(Payment.class, paymentId);
+        if (payment != null) {
+            entityManager.remove(payment);
+        } else {
+            throw new IllegalArgumentException("Payment not found with ID: " + paymentId);
+        }
+    }
 }
--- src/main/java/org/acme/services/PaymentService.java
+++ src/main/java/org/acme/services/PaymentService.java
@@ -1,2 +1,30 @@
-package org.acme.services;public class PaymentService {
+package org.acme.services;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.acme.models.Payment;
+import org.acme.repositories.PaymentRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@ApplicationScoped
+public class PaymentService {
+
+    @Inject
+    PaymentRepository paymentRepository;
+
+    public Payment create(Payment payment) {
+        return paymentRepository.create(payment);
+    }
+
+    public List<Payment> findAll() {
+        return Payment.listAll();
+    }
+
+    public void delete(UUID paymentId) {
+        paymentRepository.delete(paymentId);
+    }
+
 }
