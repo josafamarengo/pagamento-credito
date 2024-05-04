4c90ea04ef1dd780182b445b0e2800c51e058ade
Josafá Marengo <josafabmarengo@gmail.com>
2024-05-04 14:02:10 -0300

    build: add import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,11 @@
--- This file allow to write SQL commands that will be emitted in test and dev.
--- The commands are commented as their support depends of the database
--- insert into myentity (id, field) values(1, 'field-1');
--- insert into myentity (id, field) values(2, 'field-2');
--- insert into myentity (id, field) values(3, 'field-3');
--- alter sequence myentity_seq restart with 4;
+CREATE TABLE PAGAMENTOS(
+    NR_PAGAMENTO SERIAL PRIMARY KEY,
+    NR_CARTAO VARCHAR(16),
+    TIPO_PESSOA SMALLINT,
+    NR_DOCUMENTO VARCHAR(26),
+    MES_VENCIMENTO SMALLINT,
+    ANO_VENCIMENTO INTEGER,
+    CVV VARCHAR(4),
+    DATA TIMESTAMP,
+    VALOR DECIMAL
+)
